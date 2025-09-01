aa3580e1fb0e068847456393beb78a6755cbf9f1
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-09-01 18:16:46 +0300

    create - app routes

---
+++ mini_e_commerce_app/lib/app/routes/app_routes.dart
@@ -0,0 +1,7 @@
+class AppRoutes {
+  static const login = '/login';
+  static const register = '/register';
+  static const home = '/home';
+  static const product = '/product';
+  static const cart = '/cart';
+}
