ac096ac46cc2a0ea1c77af5ee249b166b2f4ae8b
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-09-03 16:13:30 +0300

    create - auth service

---
+++ mini_e_commerce_app/lib/data/services/auth_service.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+
+class AuthService extends GetxService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  Rxn<User> firebaseUser = Rxn<User>();
+
+  @override
+  void onInit() {
+    super.onInit();
+    firebaseUser.bindStream(_auth.authStateChanges());
+  }
+
+  bool get isLoggedIn => firebaseUser.value != null;
+
+  User? get currentUser => firebaseUser.value;
+}
