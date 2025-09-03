7d1152a997baea004b75fc044e12959bbf869b94
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-09-03 15:58:44 +0300

    add - firebase

--- mini_e_commerce_app/lib/main.dart
+++ mini_e_commerce_app/lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:mini_e_commerce_app/app/routes/app_pages.dart';
 import 'package:mini_e_commerce_app/app/routes/app_routes.dart';
+import 'package:mini_e_commerce_app/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -19,8 +25,8 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      initialRoute: AppRoutes.login,
-      getPages: AppPages.pages,
+      initialRoute: AppRoutes.login, // ✅ route sabitlerinden geliyor
+      getPages: AppPages.pages,      // ✅ sayfalar AppPages içinden geliyor
     );
   }
 }
