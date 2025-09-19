eb14143bede244afa9c053e522097360f48c472d
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-09-19 15:23:22 +0300

    create - color constants

---
+++ lib/core/constants/color_constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class ColorConstants {
+  static const Color primaryColor = Color(0xFF2196F3);
+  static const Color secondayColor = Color(0xFF4CAF50);
+  static const Color blackColor = Color(0xFF111111);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+}
