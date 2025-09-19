fdf311d9b0aa30e001a00dc9b63bc6c256bb4fc0
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-09-19 15:23:14 +0300

    create - app text styles

---
+++ lib/core/theme/app_text_styles.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyles {
+  // 🔹 Headings
+  static final TextStyle heading48 = GoogleFonts.openSans(
+    fontSize: 48,
+    fontWeight: FontWeight.bold,
+  );
+
+  static final TextStyle heading40 = GoogleFonts.openSans(
+    fontSize: 40,
+    fontWeight: FontWeight.bold,
+  );
+
+  static final TextStyle heading32 = GoogleFonts.openSans(
+    fontSize: 32,
+    fontWeight: FontWeight.bold,
+  );
+
+  static final TextStyle heading24 = GoogleFonts.openSans(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+
+  static final TextStyle heading20 = GoogleFonts.openSans(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  static final TextStyle heading18 = GoogleFonts.openSans(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+
+  // 🔹 Body
+  static final TextStyle bodyXLarge = GoogleFonts.openSans(
+    fontSize: 18,
+    fontWeight: FontWeight.normal,
+  );
+
+  static final TextStyle bodyLarge = GoogleFonts.openSans(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+  );
+
+  static final TextStyle bodyNormal = GoogleFonts.openSans(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+
+  static final TextStyle bodySmall = GoogleFonts.openSans(
+    fontSize: 12,
+    fontWeight: FontWeight.normal,
+  );
+
+  static final TextStyle bodyXSmall = GoogleFonts.openSans(
+    fontSize: 10,
+    fontWeight: FontWeight.normal,
+  );
+}
