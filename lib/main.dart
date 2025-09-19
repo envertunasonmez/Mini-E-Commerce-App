05fc23261a8c82353325ddc4c18487a0f04398e3
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-09-19 15:24:00 +0300

    remove - const

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Mini E - Commerce App',
       theme: ThemeData(),
-      home: const LoginView(),
+      home: LoginView(),
     );
   }
 }
