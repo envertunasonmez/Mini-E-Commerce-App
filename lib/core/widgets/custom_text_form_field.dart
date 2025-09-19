c35b1589af6174d2d46bd139ca98558df26a6511
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-09-19 15:23:41 +0300

    create - text form field

---
+++ lib/core/widgets/custom_text_form_field.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:mini_e_commerce_app/core/constants/color_constants.dart';
+import 'package:mini_e_commerce_app/core/theme/app_text_styles.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String hintText;
+  final TextEditingController controller;
+  final bool obscureText;
+  final TextInputType keyboardType;
+  final IconData? prefixIcon;
+
+  const CustomTextFormField({
+    super.key,
+    required this.hintText,
+    required this.controller,
+    this.obscureText = false,
+    this.keyboardType = TextInputType.text,
+    this.prefixIcon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      keyboardType: keyboardType,
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: AppTextStyles.bodyNormal.copyWith(
+          color: ColorConstants.blackColor,
+          fontWeight: FontWeight.w500,
+        ),
+        prefixIcon: prefixIcon != null
+            ? Icon(prefixIcon, color: Colors.grey)
+            : null,
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: Colors.grey),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: ColorConstants.primaryColor, width: 2),
+        ),
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 14,
+        ),
+      ),
+    );
+  }
+}
